f9aab226c69bb395ea9cb93cf888d64bd8d94c14
Andrew Clarke <dclarke1005@gmail.com>
2026-08-10 23:34:07 -0500

    fix(cli): separate parent and sibling import groups

--- extensions/cli/src/automation/AutomationServeAdapter.ts
+++ extensions/cli/src/automation/AutomationServeAdapter.ts
@@ -3,6 +3,7 @@ import type { Express } from "express";
 import type { ServerState } from "../commands/serve.helpers.js";
 import { messageQueue } from "../stream/messageQueue.js";
 import { BUILT_IN_TOOL_NAMES } from "../tools/builtInToolNames.js";
+
 import { registerAutomationRoutes } from "./AutomationHttpService.js";
 import type { AutomationRuntime } from "./AutomationRuntime.js";
 
